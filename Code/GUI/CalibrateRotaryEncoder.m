fdbee8cb1a3488a3da8d8610382fc6a61d86ba3d
PRIYANKA-HP\pgupta <speak2priya@gmail.com>
2017-06-30 14:39:42 -0400

    slow down the initial homing - helps avoid step skips - better calibration

--- Code/GUI/CalibrateRotaryEncoder.m
+++ Code/GUI/CalibrateRotaryEncoder.m
@@ -7,17 +7,18 @@ pause(2);
 
 % set clearpath motor control to manual override 
 handles.Arduino.write(61, 'uint16');
-pause(0.5);
+pause(2);
 
 % Move motor to one extreme and then the other
 contents = cellstr(get(handles.all_locations,'String'));  
 my_location = [str2num(char(contents(end))) 0 str2num(char(contents(1)))];
-    
+    my_location
 for i = 1:3    
     handles.Arduino.write(62, 'uint16'); % handler - move motor to specific location
     % get chosen location
+    pause(0.1);
     handles.Arduino.write(my_location(i)+101, 'uint16'); % which location
-    pause(0.5);
+    pause(1);
     % read the rotary encoder output
     temp_duration = handles.NI.DurationInSeconds;
     handles.NI.DurationInSeconds = 0.5;
