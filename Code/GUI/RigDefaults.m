adce24d38ed2cbda3629f451a66782d0266b67c3
PRIYANKA-HP\pgupta <speak2priya@gmail.com>
2018-01-23 21:48:43 -0500

    minor

--- Code/GUI/RigDefaults.m
+++ Code/GUI/RigDefaults.m
@@ -40,12 +40,8 @@ switch char(handles.computername)
         handles.MotorLocations = 120;
         handles.minimumtarget = 1;
         handles.MotorLocationsRange = 140;
-<<<<<<< HEAD
         handles.watercoeffs = [0.01181 -0.0066]; % water per drop = coeef(1)*time +coeef(2)
-=======
-        handles.watercoeffs = [0.03672 -0.2488]; % water per drop = coeef(1)*time +coeef(2)
         handles.fliphome = 0; 
->>>>>>> f6340cb1d243a97136ba38f3fdb9ae1fc2a43d2c
         
     case {'PRIYANKA-PC','DESKTOP-05QAM9D'}
         handles.file_names.Data(2) = {'C:\Data\Behavior'};
