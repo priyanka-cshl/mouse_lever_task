a0c0881cccba4ff6923f84049f88fc71ead53b94
priyanka-cshl <speak2priya@gmail.com>
2020-02-06 21:00:48 -0500

    Analysis updates : open loop vs. close loop

--- Code/Analysis2019/Ephys/WhereSpikeFile.m
+++ Code/Analysis2019/Ephys/WhereSpikeFile.m
@@ -3,7 +3,7 @@ foo = strsplit(BehaviorFile,'_');
 mousename = char(foo(1));
 date = char(foo(2));
 datetoken = [date(1:4),'-',date(5:6),'-',date(7:8)];
-root = ['/mnt/analysis/',mousename,'/'];
+root = ['/mnt/data/Priyanka/',mousename,'/'];
 myfolders = dir ([root,'/',datetoken,'*']);
 
 if size(myfolders.name,1) == 1
