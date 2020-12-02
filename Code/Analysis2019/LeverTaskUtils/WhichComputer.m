fe640ea9b7265d493ba03991cce9552bd76aaf47
priyanka gupta <speak2priya@gmail.com>
2020-12-01 20:30:52 -0500

    some changes i made for analysis at home I guess

--- Code/Analysis2019/LeverTaskUtils/WhichComputer.m
+++ Code/Analysis2019/LeverTaskUtils/WhichComputer.m
@@ -5,8 +5,10 @@ function [Paths] = WhichComputer()
 computername = deblank(computername);
 
 switch computername
-    case 'priyanka-gupta.cshl.edu'
-        Paths.Behavior = '/Volumes/Albeanu-Norepl/pgupta/Behavior'; % location on sonas server
+    case {'priyanka-gupta.cshl.edu', '*.cshl.edu'}
+        Paths.Behavior = '/Users/Priyanka/Desktop/LABWORK_II/Data/Behavior'; % local copy
+            Paths.Ephys = '/Users/Priyanka/Desktop/LABWORK_II/Data/Ephys';
+            Paths.Code = '/Users/Priyanka/Desktop/github_local';
     case {'priyanka-gupta.home', 'priyanka-gupta.local','priyanka-gupta.fios-router.home'}
         if exist('/Users/Priyanka/Desktop/LABWORK_II/Data/Behavior','dir')
             Paths.Behavior = '/Users/Priyanka/Desktop/LABWORK_II/Data/Behavior'; % local copy
@@ -22,7 +24,10 @@ switch computername
         Paths.Ephys = '/mnt/data/Priyanka';
         Paths.Code = '/opt';
     otherwise
-        Paths.Behavior = '//sonas-hs.cshl.edu/Albeanu-Norepl/pgupta/Behavior'; % location on sonas server
+        Paths.Behavior = '/Users/Priyanka/Desktop/LABWORK_II/Data/Behavior'; % local copy
+            Paths.Ephys = '/Users/Priyanka/Desktop/LABWORK_II/Data/Ephys';
+            Paths.Code = '/Users/Priyanka/Desktop/github_local';
+        %Paths.Behavior = '//sonas-hs.cshl.edu/Albeanu-Norepl/pgupta/Behavior'; % location on sonas server
 end
 
 end
