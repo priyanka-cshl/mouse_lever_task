e03a6634128eecb7f67373cf71beac07be4e28ed
DESKTOP-05QAM9D\priyanka <speak2priya@gmail.com>
2021-08-26 12:04:43 -0400

    old stuff

--- Code/extras/Due_pi_I2C_test/Due_pi_I2C_test.ino
+++ Code/extras/Due_pi_I2C_test/Due_pi_I2C_test.ino
@@ -1,31 +1,35 @@
-#include <Wire.h> // library for I2C communication - to slave Arduinos
+#include <Wire.h>
 #include <DueTimer.h>
 
 const byte SDA_pin = 20;
 const byte SCL_pin = 21;
-int pi_i2c_address = 0x08;
-byte piVal = 0;
+int motor1_i2c_address = 0x13;
+byte Val2Send = 0;
+bool flip = false;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
-  Serial.begin(115200);
-  Timer3.attachInterrupt(WriteI2C);
-  Timer3.start(1000*10); // Calls every 10 msec
+
+  Timer3.attachInterrupt(MoveBar);
+  Timer3.start(1000 * 1000); // Calls every 10 msec
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // I2Cwriter(pi_i2c_address,piVal);
-//  piVal = (piVal + 1);
-//  Serial.println(piVal);
-//  delay(1);
 }
 
-void WriteI2C() {
-  piVal = piVal + 1;
-  I2Cwriter(pi_i2c_address,piVal);
-  Serial.println(piVal);
+void MoveBar() {
+    if (flip)
+    {
+      I2Cwriter(motor1_i2c_address,1);
+    }
+    else
+    {
+      I2Cwriter(motor1_i2c_address,1);
+    }
+  flip = !flip;
 }
 
 void I2Cwriter (int wire_address, int DataToWrite)
