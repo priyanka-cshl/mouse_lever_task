cfc72ece4f047c689d41592eea616687443a36c5
priyanka-cshl <speak2priya@gmail.com>
2021-11-09 13:15:46 -0500

    Moved all nrelevant analysis code to a different repository - SmellocatorAnalysis

--- Code/Analysis2021/LeverTaskUtils/WhereTuningFile.m
+++
@@ -1,8 +0,0 @@
-function [TuningFile] = WhereTuningFile(FilePaths,BehaviorFile)
-TuningFile = [regexprep(BehaviorFile,'_r','_o'),'.mat'];
-if exist(fullfile(FilePaths,TuningFile),'file')
-    TuningFile = fullfile(FilePaths,TuningFile);
-else
-    TuningFile = [];
-end
-end
